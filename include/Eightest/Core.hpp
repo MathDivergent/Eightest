367e8f623bac82304bcc3dee45f3b5e3c7937870
MathDivergent <71389444+MathDivergent@users.noreply.github.com>
2026-02-21 23:52:10 +0200

    Upload Eightest v2.10.0.

---
+++ include/Eightest/Core.hpp
@@ -0,0 +1,159 @@
+#ifndef EIGHTEST_CORE_HPP
+#define EIGHTEST_CORE_HPP
+
+#include <utility> // forward
+#include <functional> // function
+#include <unordered_map> // unordered_map
+#include <string> // string
+#include <sstream> // stringstream
+
+
+// Test definition
+
+// Allow to hide owner classes within different translation units
+#define TEST_SPACE(...) namespace __VA_ARGS__
+
+#define TEST(test_module, test_name, ...) \
+    TEST_SPACE(test_module) { \
+        static struct test_name : eightest::test_t { \
+            test_name() : eightest::test_t(#test_module, #test_name) { bind(__VA_ARGS__); } \
+            void run() override; \
+        } s##test_name; \
+    } \
+    void test_module::test_name::run()
+
+// Calculate completed and failed test, provide debug info
+#define EXPECT(msg, ...) this->registry->check(__VA_ARGS__, this, msg)
+#define ASSERT(msg, ...) if(!EXPECT(msg, __VA_ARGS__)) return
+
+// Will allow to show expression by text printer
+#define EXPR(...) eightest::expression(__VA_ARGS__)
+
+
+namespace eightest
+{
+
+template <typename T>
+struct expression_t
+{
+    T value;
+    std::string string_value;
+
+    expression_t<bool> operator!() const;
+};
+
+template <typename T>
+struct expression_t<T&>
+{
+    T& value;
+    std::string string_value;
+
+    expression_t<bool> operator!() const;
+};
+
+template <typename T>
+expression_t<T> expression(T&& expression_value, std::string const& expression_string_value)
+{
+    return expression_t<T>{std::forward<T>(expression_value), expression_string_value};
+}
+
+template <typename T>
+expression_t<T> expression(T&& expression_value)
+{
+    std::string expression_string_value;
+    std::stringstream stream; stream << expression_value; stream >> expression_string_value;
+    return expression(std::forward<T>(expression_value), expression_string_value);
+}
+
+EIGHTEST_API expression_t<std::nullptr_t> expression(std::nullptr_t expression_value);
+EIGHTEST_API expression_t<bool> expression(bool expression_value);
+
+template <typename T>
+expression_t<bool> expression_t<T>::operator!() const
+{
+    return expression(!static_cast<bool>(value), "!(" + string_value + ")");
+}
+
+class registry_t;
+
+class EIGHTEST_API test_t
+{
+public:
+    std::string const module;
+    std::string const name;
+    registry_t* registry = nullptr;
+
+public:
+    test_t(std::string const& module, std::string const& name);
+
+public:
+    virtual void run() = 0;
+
+protected:
+    void bind(registry_t* to = nullptr);
+};
+
+class EIGHTEST_API registry_t
+{
+public:
+    std::unordered_map<std::string, std::unordered_map<std::string, test_t*>> all;
+    std::function<void(std::string const&)> stat_handler = &default_stat_handler;
+
+public:
+    std::size_t passed = 0;
+    std::size_t failed = 0;
+
+public:
+    void add(test_t* test);
+
+public:
+    bool check(bool condition, test_t* test, std::string const& msg);
+    bool check(expression_t<bool> const& expression, test_t* test, std::string const& msg);
+    void safe_run(test_t* test);
+
+public:
+    void execute_module(std::string const& name); // execute test module with specify name
+    void execute_test(std::string const& name); // execute tests with specify name
+    void execute_all();
+
+public:
+    bool stat(); // return true in all passed
+    static void default_stat_handler(std::string const& text);
+};
+
+extern EIGHTEST_API registry_t* global();
+
+} // namespace eightest
+
+#define EIGHTEST_EXPRESSION_OPERATOR_GENERIC(op, lhs_brace, rhs_brace) \
+    template <typename L, typename R> \
+    eightest::expression_t<bool> operator op(eightest::expression_t<L> const& lhs_expression, \
+                                             eightest::expression_t<R> const& rhs_expression) { \
+        return eightest::expression( \
+            lhs_expression.value op rhs_expression.value, \
+            lhs_brace+lhs_expression.string_value+" "#op" "+rhs_expression.string_value+rhs_brace \
+        ); \
+    } \
+    template <typename L, typename R> \
+    eightest::expression_t<bool> operator op(eightest::expression_t<L> const& lhs_expression, \
+                                             R const& rhs_value) { \
+        return ::operator op(lhs_expression, eightest::expression(rhs_value)); \
+    } \
+    template <typename L, typename R> \
+    eightest::expression_t<bool> operator op(L const& lhs_value, \
+                                             eightest::expression_t<R> const& rhs_expression) { \
+       return ::operator op(eightest::expression(lhs_value), rhs_expression); \
+    }
+
+EIGHTEST_EXPRESSION_OPERATOR_GENERIC(==,"","")
+EIGHTEST_EXPRESSION_OPERATOR_GENERIC(!=,"","")
+EIGHTEST_EXPRESSION_OPERATOR_GENERIC(<,"","")
+EIGHTEST_EXPRESSION_OPERATOR_GENERIC(<=,"","")
+EIGHTEST_EXPRESSION_OPERATOR_GENERIC(>,"","")
+EIGHTEST_EXPRESSION_OPERATOR_GENERIC(>=,"","")
+EIGHTEST_EXPRESSION_OPERATOR_GENERIC(&&,"(",")")
+EIGHTEST_EXPRESSION_OPERATOR_GENERIC(||,"(",")")
+
+#undef EIGHTEST_EXPRESSION_OPERATOR_GENERIC
+
+#endif // EIGHTEST_CORE_HPP
---
+++ src/Eightest/Core.cpp
@@ -0,0 +1,161 @@
+#ifdef EIGHTEST_DEFAULT_STAT_HANDLER
+#include <cstdio> // printf
+#endif // EIGHTEST_DEFAULT_STAT_HANDLER
+
+#include <Eightest/Core.hpp>
+
+namespace eightest
+{
+
+expression_t<std::nullptr_t> expression(std::nullptr_t expression_value)
+{
+    return expression_t<std::nullptr_t>{expression_value, "nullptr"};
+}
+
+expression_t<bool> expression(bool expression_value)
+{
+    return expression_t<bool>{expression_value, expression_value == true ? "true" : "false"};
+}
+
+test_t::test_t(std::string const& module, std::string const& name)
+    : module(module), name(name)
+{
+}
+
+void test_t::bind(registry_t* to)
+{
+    registry = to;
+
+    if (registry == nullptr)
+    {
+        registry = global();
+    }
+
+    registry->add(this);
+}
+
+static std::string info_format(test_t* test, std::string const& msg, std::string const& string_value, bool ok)
+{
+    return (ok ? "[   OK   ] " : "[ FAILED ] ")
+         + std::string(test->module)
+         + "::"
+         + test->name
+         + '.'
+         + msg
+         + (ok ? "\n" : " [  WITH  ] " + string_value + "\n");
+}
+
+static void update_stat(std::size_t& passed, std::size_t& failed, bool ok)
+{
+    passed += ok;
+    failed += not ok;
+}
+
+void registry_t::add(test_t* test)
+{
+    auto& place = all[test->module][test->name];
+    if (place == nullptr)
+    {
+        place = test;
+    }
+    else
+    {
+        stat_handler(std::string("[ COPIED ]") + test->module + "::" + test->name);
+        failed += 1;
+    }
+}
+
+bool registry_t::check(bool condition, test_t* test, std::string const& msg)
+{
+    return check(expression(condition), test, msg);
+}
+
+bool registry_t::check(expression_t<bool> const& expression, test_t* test, std::string const& msg)
+{
+    auto const condition = static_cast<bool>(expression.value);
+    update_stat(passed, failed, condition);
+    stat_handler(info_format(test, msg, expression.string_value, condition));
+    return condition;
+}
+
+void registry_t::safe_run(test_t* test)
+{
+    try
+    {
+        test->run();
+    }
+    catch(char const* e)
+    {
+        check(expression_t<bool>{false, e}, test, "<exception>");
+    }
+    catch(std::exception const& e)
+    {
+        check(expression_t<bool>{false, e.what()}, test, "<exception>");
+    }
+    catch(...)
+    {
+        check(expression_t<bool>{false, "<unknown>"}, test, "<exception>");
+    }
+}
+
+void registry_t::execute_module(std::string const& name)
+{
+    auto it = all.find(name);
+    if (it == all.end()) return;
+
+    auto& module = it->second;
+    for (auto& name_test : module) safe_run(name_test.second);
+}
+
+void registry_t::execute_test(std::string const& name)
+{
+    for (auto& name_module : all)
+    {
+        auto& module = name_module.second;
+
+        auto it = module.find(name);
+        if (it == module.end()) continue;
+
+        safe_run(it->second);
+    }
+}
+
+void registry_t::execute_all()
+{
+    for (auto& name_module : all)
+    {
+        auto& module = name_module.second;
+        for (auto& name_test : module) safe_run(name_test.second);
+    }
+}
+
+static std::string stat_format(unsigned passed, unsigned failed)
+{
+    return "\nOK: "
+         + std::to_string(passed)
+         + " FAILED: "
+         + std::to_string(failed)
+         + " TOTAL: "
+         + std::to_string(passed+failed)
+         + '\n';
+}
+
+bool registry_t::stat()
+{
+    stat_handler(stat_format(passed, failed));
+    return failed == 0;
+}
+
+void registry_t::default_stat_handler(std::string const& stat)
+{
+    #ifdef EIGHTEST_DEFAULT_STAT_HANDLER
+    printf("%s", stat.c_str());
+    #endif // EIGHTEST_DEFAULT_STAT_HANDLER
+}
+
+registry_t* global()
+{
+    static registry_t self; return &self;
+}
+
+} // namespace eightest
